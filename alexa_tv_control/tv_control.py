f6c2d88a32a4a497d8254afefd10595d467fd33e
Roshun Alur <roshun.alur@gmail.com>
2016-12-10 04:34:30 +0000

    adding *.pyc and *.swp to gitignore

---
+++ alexa_tv_control/tv_control.py
@@ -0,0 +1,48 @@
+""" based on fauxmo_minimal.py by Fabricate.IO
+    git repo: https://github.com/toddmedema/echo/
+"""
+
+import fauxmo
+import logging
+import time
+import subprocess
+
+from debounce_handler import debounce_handler
+
+logging.basicConfig(level=logging.CRITICAL) # Only print CRITICAL messages
+
+class device_handler(debounce_handler):
+    """Handles requests from Alexa"""
+    # For this project, we're using the trigger "the tv" and listening on port 52000
+    TRIGGERS = {"the tv": 52000}
+
+    # Function to perform a series of actions when a trigger is received from Alexa
+    def act(self, client_address, state):
+        logging.debug("State", state, "from client @", client_address)
+        # Open subprocess to send IR command using irsend
+        subprocess.Popen("irsend SEND_ONCE vizio KEY_POWER", shell=True) 
+        return True
+
+if __name__ == "__main__":
+    # Startup the fauxmo server
+    fauxmo.DEBUG = True
+    p = fauxmo.poller()
+    u = fauxmo.upnp_broadcast_responder()
+    u.init_socket()
+    p.add(u)
+
+    # Register the device callback as a fauxmo handler
+    d = device_handler()
+    for trig, port in d.TRIGGERS.items():
+        fauxmo.fauxmo(trig, u, p, None, port, d)
+
+    # Loop and poll for incoming Echo requests
+    logging.debug("Entering fauxmo polling loop")
+    while True:
+        try:
+            # Allow time for a ctrl-c to stop the process
+            p.poll(100)
+            time.sleep(0.1)
+        except Exception, e:
+            logging.critical("Critical exception: " + str(e))
+            break
