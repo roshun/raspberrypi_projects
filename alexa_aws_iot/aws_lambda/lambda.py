481b3c11a6784c1a16026c893ae62f1b27d3097c
Roshun Alur <roshun.alur@gmail.com>
2017-01-18 17:13:01 +0000

    latest aws lambda code

--- alexa_aws_iot/aws_lambda/lambda.py
+++ alexa_aws_iot/aws_lambda/lambda.py
@@ -4,8 +4,15 @@ import random
 
 myAWSIoTMQTTClient = AWSIoTMQTTClient("basicPubSub")
 
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
 def lambda_handler(event, context):
-    
     mqttSetup(myAWSIoTMQTTClient)
     
     if (event["session"]["application"]["applicationId"] !=
@@ -40,13 +47,10 @@ def mqttSetup(MQTTClient):
     MQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
 
     ## AWSIoTMQTTClient connection configuration
-    MQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-    MQTTClient.configureDrainingFrequency(100)  # Draining: 100 Hz
-    MQTTClient.configureConnectDisconnectTimeout(5)  # 5 sec
-    MQTTClient.configureMQTTOperationTimeout(3)  # 3 sec
-
-    # Connect to AWS IoT
-    MQTTClient.connect()
+    MQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    MQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+    MQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    MQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
     return True
 
@@ -59,31 +63,133 @@ def on_launch(launch_request, session):
 def on_intent(intent_request, session):
     intent = intent_request["intent"]
     intent_name = intent_request["intent"]["name"]
-    intent_value = intent_request["intent"]["slots"]["power_state"]["value"]
+    if intent_name != "clear":
+        try:
+            intent_type = "power_state"
+            intent_value = intent_request["intent"]["slots"]["power_state"]["value"]
+        except KeyError:
+            try:
+                intent_type = "video_service"
+                intent_value = intent_request["intent"]["slots"]["video_service"]["value"]
+            except KeyError:
+                try:
+                    intent_type = "button"
+                    intent_value = intent_request["intent"]["slots"]["button"]["value"]
+                except KeyError:
+                    try:
+                        intent_type = "direction"
+                        intent_value = intent_request["intent"]["slots"]["direction"]["value"]
+                    except KeyError:
+                        try:
+                            intent_type = "search"
+                            intent_value = intent_request["intent"]["slots"]["search"]["value"]
+                        except KeyError:
+                            intent_type = "volume_direction"
+                            intent_value = intent_request["intent"]["slots"]["volume_direction"]["value"]
+                            try:
+                                volume_amount = intent_request["intent"]["slots"]["volume_amount"]["value"]
+                            except KeyError:
+                                volume_amount = 1
 
-    if intent_name == "TV":
-        if intent_value == "on":
-            card_title = "TV Remote on"
-            speech_output = "Turning on your television"
+    if intent_name == "AppleTV":
+        if intent_type == "button":
+            card_title = "Apple TV Remote press " + intent_value
+            speech_output = "Pressing " + intent_value + " on your Apple TV"
             should_end_session = True
             # Publish to MQTT here
             myAWSIoTMQTTClient.connect()
-            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/tvRemote", "Turn on", 1)
+            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/appleTvRemote", intent_value, 1)
+            myAWSIoTMQTTClient.subscribe("$aws/things/RaspberryPi/appleTRemote", 1, customCallback)
+            myAWSIoTMQTTClient.unsubscribe("$aws/things/RaspberryPi/appleTvRemote")
+            myAWSIoTMQTTClient.disconnect()
             # TODO: make sure the update was registered by Raspberry Pi
             return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
-        elif intent_value == "off":
-            card_title = "TV Remote off"
-            speech_output = "Turning off your television"
+        elif intent_type == "direction":            
+            card_title = "Apple TV Remote move " + intent_value
+            speech_output = "Moving " + intent_value + " on your Apple TV"
+            should_end_session = True
             # Publish to MQTT here
             myAWSIoTMQTTClient.connect()
-            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/tvRemote", "Turn off", 1)
+            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/appleTvRemote", intent_value, 1)
+            myAWSIoTMQTTClient.subscribe("$aws/things/RaspberryPi/appleTvRemote", 1, customCallback)
+            myAWSIoTMQTTClient.unsubscribe("$aws/things/RaspberryPi/appleTvRemote")
+            myAWSIoTMQTTClient.disconnect()
             # TODO: make sure the update was registered by Raspberry Pi
-            should_end_session = True
             return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
         else:
             raise ValueError("Invalid power state")
+    elif intent_name == "Vizio":
+        if intent_type == "button":
+            card_title = "Vizio TV Remote press " + intent_value
+            speech_output = "Pressing " + intent_value + " on your Vizio TV"
+            should_end_session = True
+            # Publish to MQTT here
+            myAWSIoTMQTTClient.connect()
+            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/vizioTvRemote", intent_type + " " + intent_value, 1)
+            myAWSIoTMQTTClient.subscribe("$aws/things/RaspberryPi/vizioTRemote", 1, customCallback)
+            myAWSIoTMQTTClient.unsubscribe("$aws/things/RaspberryPi/vizioRemote")
+            myAWSIoTMQTTClient.disconnect()
+            # TODO: make sure the update was registered by Raspberry Pi
+            return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
+        elif intent_type == "power_state":
+            card_title = "Vizio TV Remote turn " + intent_value
+            speech_output = "Turning " + intent_value + " your Vizio TV"
+            should_end_session = True
+            # Publish to MQTT here
+            myAWSIoTMQTTClient.connect()
+            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/vizioTvRemote", intent_type + " " + intent_value, 1)
+            myAWSIoTMQTTClient.subscribe("$aws/things/RaspberryPi/vizioTvRemote", 1, customCallback)
+            myAWSIoTMQTTClient.unsubscribe("$aws/things/RaspberryPi/vizioTvRemote")
+            myAWSIoTMQTTClient.disconnect()
+            # TODO: make sure the update was registered by Raspberry Pi
+            return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
+        elif intent_type == "volume_direction":
+            card_title = "Vizio TV Remote turning volume " + intent_value + " by " + str(volume_amount) 
+            speech_output = "Turning the volume " + intent_value + " by " + str(volume_amount) + " on your Vizio TV"
+            should_end_session = True
+            myAWSIoTMQTTClient.connect()
+            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/vizioTvRemote", "volume" + " " + intent_value + " " + str(volume_amount), 1)
+            myAWSIoTMQTTClient.subscribe("$aws/things/RaspberryPi/vizioTvRemote", 1, customCallback)
+            myAWSIoTMQTTClient.unsubscribe("$aws/things/RaspberryPi/vizioTvRemote")
+            myAWSIoTMQTTClient.disconnect()
+            # TODO: make sure the update was registered by Raspberry Pi
+            return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
+        elif intent_type == "search":
+            card_title = "Vizio TV Remote search for " + intent_value
+            speech_output = "Searching for " + intent_value + " on your Vizio TV"
+            should_end_session = True
+            myAWSIoTMQTTClient.connect()
+            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/vizioTvRemote", intent_type + " " + intent_value, 1)
+            myAWSIoTMQTTClient.subscribe("$aws/things/RaspberryPi/vizioTvRemote", 1, customCallback)
+            myAWSIoTMQTTClient.unsubscribe("$aws/things/RaspberryPi/vizioTvRemote")
+            myAWSIoTMQTTClient.disconnect()
+            # TODO: make sure the update was registered by Raspberry Pi
+            return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
+        elif intent_type == "video_service":
+            card_title = "Vizio TV Remote opening video service " + intent_value
+            speech_output = "Opening the " + intent_value + " app on your Vizio TV"
+            should_end_session = True
+            myAWSIoTMQTTClient.connect()
+            myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/vizioTvRemote", intent_type + " " + intent_value, 1)
+            myAWSIoTMQTTClient.subscribe("$aws/things/RaspberryPi/vizioTvRemote", 1, customCallback)
+            myAWSIoTMQTTClient.unsubscribe("$aws/things/RaspberryPi/vizioTvRemote")
+            myAWSIoTMQTTClient.disconnect()
+            # TODO: make sure the update was registered by Raspberry Pi
+            return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
+    elif intent_name == "clear":
+        card_title = "Vizio TV remote: clearing the search bar"
+        speech_output = "Clearing the search bar on your Vizio TV"
+        should_end_session = True
+        # Publish to MQTT here
+        myAWSIoTMQTTClient.connect()
+        myAWSIoTMQTTClient.publish("$aws/things/RaspberryPi/vizioTvRemote", intent_name, 1)
+        myAWSIoTMQTTClient.subscribe("$aws/things/RaspberryPi/vizioTRemote", 1, customCallback)
+        myAWSIoTMQTTClient.unsubscribe("$aws/things/RaspberryPi/vizioRemote")
+        myAWSIoTMQTTClient.disconnect()
+        # TODO: make sure the update was registered by Raspberry Pi
+        return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
     else:
-        raise ValueError("Invalid intent")
+        raise ValueError("Invalid intent: " + intent_name)
 
 def get_welcome_response():
     session_attributes = {}
@@ -120,4 +226,3 @@ def build_response(session_attributes, speechlet_response):
         "sessionAttributes": session_attributes,
         "response": speechlet_response
     }
-
